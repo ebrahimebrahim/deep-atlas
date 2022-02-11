ee8c27000204819f422ba699e3f2cb478fe03c75
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2022-02-11 09:07:02 -0500

    Fix 2D vector field plotting component flip

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ import monai
 def preview_image(image_array, normalize_by = "volume", cmap = None, figsize = (12,12), threshold = None):
     """
     Display three orthogonal slices of the given 3D image.
-    
+
     image_array is assumed to be of shape (H,W,D)
-    
+
     If a number is provided for threshold, then pixels for which the value
     is below the threshold will be shown in red
     """
@@ -19,51 +19,58 @@ def preview_image(image_array, normalize_by = "volume", cmap = None, figsize = (
         vmax = image_array.max().item()
     else :
         raise(ValueError(f"Invalid value '{normalize_by}' given for normalize_by"))
-    
+
     # half-way slices
     x,y,z = np.array(image_array.shape)//2
     imgs = (image_array[x,:,:], image_array[:,y,:], image_array[:,:,z])
-    
+
     fig, axs = plt.subplots(1,3,figsize=figsize)
     for ax,im in zip(axs,imgs):
         ax.axis('off')
         ax.imshow(im, origin = 'lower', vmin = vmin, vmax = vmax, cmap=cmap)
-        
+
         # threshold will be useful when displaying jacobian determinant images;
         # we will want to clearly see where the jacobian determinant is negative
         if threshold is not None:
             red = np.zeros(im.shape+(4,)) # RGBA array
             red[im<=threshold] = [1,0,0,1]
             ax.imshow(red, origin = 'lower')
-        
+
     plt.show()
 
 
 def plot_2D_vector_field(vector_field, downsampling):
-    """vector_field should be a tensor of shape (2,H,W)"""
+    """vector_field should be a tensor of shape (2,H,W).
+
+    The plot origin will be in the lower left.
+    Let's use "x" and "y" for the rightward and upward directions respectively;
+    then the vector at location (x,y) in the plot image will have
+    vector_field[1,y,x] as its x-component and
+    vector_field[0,y,x] as its y-component.
+    """
     downsample2D = monai.networks.layers.factories.Pool['AVG',2](kernel_size=downsampling)
     vf_downsampled = downsample2D(vector_field.unsqueeze(0))[0]
     plt.quiver(
-        vf_downsampled[0,:,:], vf_downsampled[1,:,:],
+        vf_downsampled[1,:,:], vf_downsampled[0,:,:],
         angles='xy', scale_units='xy', scale=downsampling,
         headwidth=4.
-    );
-    
+    )
+
 
 def preview_3D_vector_field(vector_field, downsampling=None):
     """
     Display three orthogonal slices of the given 3D vector field.
-    
+
     vector_field should be a tensor of shape (3,H,W,D)
-    
+
     Vectors are projected into the viewing plane, so you are only seeing
     their components in the viewing plane.
     """
-    
+
     if downsampling is None:
         # guess a reasonable downsampling value to make a nice plot
         downsampling = max(1, int(max(vector_field.shape[1:])) >> 5 )
-    
+
     x,y,z = np.array(vector_field.shape[1:])//2 # half-way slices
     plt.figure(figsize=(18,6))
     plt.subplot(1,3,1); plt.axis('off')
@@ -78,7 +85,7 @@ def plot_2D_deformation(vector_field, grid_spacing, **kwargs):
     """
     Interpret vector_field as a displacement vector field defining a deformation,
     and plot an x-y grid warped by this deformation.
-    
+
     vector_field should be a tensor of shape (2,H,W)
         Note: vector_field spatial indices are swapped to match the conventions of imshow and quiver
     kwargs are passed to matplotlib plotting
@@ -106,10 +113,10 @@ def preview_3D_deformation(vector_field, grid_spacing, **kwargs):
     """
     Interpret vector_field as a displacement vector field defining a deformation,
     and plot warped grids along three orthogonal slices.
-    
+
     vector_field should be a tensor of shape (3,H,W,D)
     kwargs are passed to matplotlib plotting
-    
+
     Deformations are projected into the viewing plane, so you are only seeing
     their components in the viewing plane.
     """
@@ -127,18 +134,18 @@ def preview_3D_deformation(vector_field, grid_spacing, **kwargs):
 def jacobian_determinant(vf):
     """
     Given a displacement vector field vf, compute the jacobian determinant scalar field.
-    
+
     vf is assumed to be a vector field of shape (3,H,W,D),
     and it is interpreted as the displacement field.
     So it is defining a discretely sampled map from a subset of 3-space into 3-space,
     namely the map that sends point (x,y,z) to the point (x,y,z)+vf[:,x,y,z].
     This function computes a jacobian determinant by taking discrete differences in each spatial direction.
-    
+
     Returns a numpy array of shape (H-1,W-1,D-1).
     """
 
     _, H, W, D = vf.shape
-    
+
     # Compute discrete spatial derivatives
     diff_and_trim = lambda array, axis : np.diff(array, axis=axis)[:,:(H-1),:(W-1),:(D-1)]
     dx = diff_and_trim(vf, 1)
@@ -158,7 +165,7 @@ def jacobian_determinant(vf):
 def plot_against_epoch_numbers(epoch_and_value_pairs, **kwargs):
     """
     Helper to reduce code duplication when plotting quantities that vary over training epochs
-    
+
     epoch_and_value_pairs: An array_like consisting of pairs of the form (<epoch number>, <value of thing to plot>)
     kwargs are forwarded to matplotlib.pyplot.plot
     """
