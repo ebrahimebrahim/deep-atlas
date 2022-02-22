94460d85e3b5555556a2f02610dc2ab72c76d80b
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2022-02-21 19:07:52 -0500

    make generate_toc take a notebook as argument

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ An exploration of the DeepAtlas registration and segmentation methodology
 The main attraction is `deep_atlas_tutorial.ipynb`.
 
 Jupyter notebooks cannot automatically generate a table of contents,
-so if you edit the headings then you can run `generate_toc.sh`
+so if you edit the headings then you can run `generate_toc.sh deep_atlas_tutorial.ipynb`
 to generate a new TOC.
--- generate_toc.sh
+++ generate_toc.sh
@@ -2,9 +2,13 @@
 # generate the table of contents for deep atlas tutorial notebook, and print to stdout
 # need jupyter notebook and the python package markdown-toc
 
-
 set -e
 
-jupyter nbconvert --to markdown --output temp.md deep_atlas_tutorial.ipynb
+if [ $# -lt 1 ]; then 
+  echo "need to specify notebook file to create TOC for"
+  exit
+fi
+
+jupyter nbconvert --to markdown --output temp.md $1
 markdown-toc -h 6 -t github --no-write temp.md
 rm temp.md
